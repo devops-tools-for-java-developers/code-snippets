021c173dbaeed0abe63cff238c3afeb30e4d8f91
Ixchel Ruiz <ixchelruiz@ixchelrjfrog.local>
2021-12-23 21:25:40 +0100

    Chapter 04 and Chapter 06

---
+++ chapter04-dissecting-the-monolith/lambda/quarkus-lambda/src/main/java/com/example/demo/OutputObject.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class OutputObject {
+    private String result;
+    private String requestId;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+}
